26863dc02cd72a9efee183e423b5a1526bb53179
Dan Souza <souza.music@outlook.com>
2022-02-02 21:31:22 -0800

    assignment update

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){ 
+let feet = ride
+if (feet <= 400){
+  return 'This one is on me!'
+  }
+else if (feet >= 2000 && feet <= 2500){
+  return 'I will gladly take your thirty bucks.'
+  }
+else {
+  return 'No can do.'
+  }
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+let ct = city
+  if (ct === `NYC`){
+    return 'Ok, sounds good.'
+  }
+  else {
+    return 'No go.'
+  }
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+const generous = tip
+switch (generous) {
+  case "the tip is generous":
+    console.log("Thank you so much.");
+    break;
+  case "the tip is not as generous":
+    console.log("Thank you.");
+    break;
+  default:
+    console.log("Bye.");
+    break;
+  }
 }
